8c450412d322051ac3fb855576af5ba7eb1de0dc
Micha Niskin <asdf@ubergibson.com>
2008-10-13 04:12:14 +0000

    Initial checkin for golf!
    git-svn-id: svn+ssh://ubergibson.com/var/svn/repos/golf@1 9708d6e4-2587-447f-a9e4-23e61c6ca176

---
+++ golf-start.sh
@@ -0,0 +1,2 @@
+#\!/usr/bin/env bash
+java -jar /home/micha/work/golf/dist/golf-0.1-dev.jar "$@"
---
+++ src/com/ubergibson/golf/Main.java
@@ -0,0 +1,6 @@
+package com.ubergibson.golf;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("golf says hi!");
+  }
+}
