7a994e65907ba37a57a8d98527db21e23a478203
Micha Niskin <asdf@ubergibson.com>
2008-11-14 19:22:22 +0000

    git-svn-id: svn+ssh://ubergibson.com/var/svn/repos/golf@4 9708d6e4-2587-447f-a9e4-23e61c6ca176

---
+++ src/com/ubergibson/golf/GolfServlet.java
@@ -0,0 +1,366 @@
+package com.ubergibson.golf;
+
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.mozilla.javascript.*;
+import org.golfscript.js.*;
+import org.mortbay.log.Log;
+
+public class GolfServlet extends HttpServlet {
+  
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+  {
+    if (request.getSession(false) == null) {
+      HttpSession s = request.getSession(true);
+      Log.info(fmtLogMsg(request, "NEW SESSION"));
+    }
+
+    logRequest(request);
+
+    PrintWriter       out         = response.getWriter();
+    ContextFactory    cf          = ContextFactory.getGlobal();
+    Context           cx          = cf.enterContext();
+    HttpSession       session     = request.getSession();
+    String            initialDOM  = (String) session.getAttribute("initialDOM");
+    ScriptableObject  myScope     = (ScriptableObject)
+                                      session.getAttribute("myScope");
+    String            event       = request.getParameter("event");
+    String            target      = request.getParameter("target");
+    String            contextPath = request.getContextPath();
+    String            pathInfo    = request.getPathInfo();
+    String            docRoot     = getServletContext().getRealPath("");
+    String            finalDOM    = "";
+    Scriptable        scope;
+    Object            res;
+
+    /*
+     * 2 possible cases:
+     *   1) path doesn't end in a '/' character
+     *   2) path ends in '/' character
+     *
+     * First case  --> raw file retreival.
+     * Second case --> routed component entry point.
+     */
+    
+    /* First case */
+    if (!pathInfo.endsWith("/")) {
+
+      /* Resources in jar file and stuff in servlet docroot. */
+      try {
+        String path[] = pathInfo.split("//*");
+        if (path.length > 0) {
+          String file = path[path.length - 1];
+          String content = getGolfResourceAsString(file);
+          if (content.length() > 0) {
+            if (file.endsWith(".js"))
+              response.setContentType("text/javascript");
+            else if (file.endsWith(".css"))
+              response.setContentType("text/css");
+            else
+              response.setContentType("text/plain");
+            Log.info(fmtLogMsg(request, "static content `" + pathInfo + "'"));
+            out.println(content);
+            return;
+          }
+        }
+      }
+
+      catch (Exception x) {
+        // no worries
+      }
+
+      /* Files in the routed component dirs (for direct access). */
+      try {
+        String file = pathInfo;
+        String content = getGolfResourceAsString(file);
+        if (content.length() > 0) {
+          if (pathInfo.endsWith(".js"))
+            response.setContentType("text/javascript");
+          else if (file.endsWith(".css"))
+            response.setContentType("text/css");
+          else
+            response.setContentType("text/plain");
+          Log.info(fmtLogMsg(request, "static content `" + pathInfo + "'"));
+          out.println(content);
+          return;
+        }
+      }
+
+      catch (Exception x) {
+        // no worries
+      }
+
+      /* No files found: maybe '/' was omitted by mistake?
+         Try redirecting to routed entry point. */
+      String qs = 
+        (request.getQueryString() == null ? "" : request.getQueryString());
+      String uri = request.getRequestURI() + "/" + qs;
+      sendRedirect(request, response, uri);
+      return;
+    }
+
+    /* Second case */
+    try {
+      response.setContentType("text/html");
+
+      /* Client-server sync can be lost e.g. when server is restarted */
+      if ((event != null || target != null) 
+          && (initialDOM == null || initialDOM.length() < 30)) {
+        Log.warn(fmtLogMsg(request, "lost sync (event with no associated VM)"));
+        sendRedirect(request, response, request.getRequestURI());
+      }
+
+      /* No event --> fresh VM */
+      if ((event == null && target == null) || initialDOM.length() < 30
+          || event == null || target == null) {
+        session.setAttribute("initialDOM", null);
+        session.setAttribute("myScope", null);
+        myScope = null;
+        initialDOM = getGolfResourceAsString("new.html");
+      } 
+
+      if (myScope == null) {
+        /* Fresh VM */
+        Log.info(fmtLogMsg(request, "creating a new VM"));
+
+        myScope = new Global();
+        scope = cx.initStandardObjects(myScope);
+        ScriptableObject.defineClass(scope, File.class);
+
+        ScriptableObject.putProperty(scope, "serverside", new Boolean(true));
+
+        String componentPath = getServletContext().getRealPath("/components" + 
+              getPathRouted(pathInfo));
+
+        Scriptable golfObj = cx.newObject(scope, "Object");
+        ScriptableObject.putProperty(golfObj, "initialDOM", initialDOM);
+        ScriptableObject.putProperty(golfObj, "componentPath", componentPath);
+        scope.put("Golf", scope, golfObj);
+
+        res = runScript(cx, scope, "dom.js");
+        res = runScript(cx, scope, "jquery.js");
+        res = runScript(cx, scope, "golf.js");
+        res = cx.evaluateString(scope,"Golf.load()","golf",0,null);
+      } else {
+        /* Re-use existing VM */ 
+        Log.info(fmtLogMsg(request, "re-using an existing VM"));
+
+        scope = myScope;
+
+        /* Verify that VM is not braindead */
+        res = cx.evaluateString(scope,"Golf.ping()","golf",0,null);
+        if ( !Context.toString(res).equals("ok") ) {
+          Log.warn(fmtLogMsg(request, 
+                "ping->not ok: VM appears to be braindead!"));
+          sendRedirect(request, response, request.getRequestURI());
+          return;
+        }
+
+        /* Import the event into the VM */
+        ScriptableObject.putProperty(scope, "golfTarget", target);
+        ScriptableObject.putProperty(scope, "golfEvent",  event);
+
+        /* Import the saved html of the page into the VM as Golf.initialDOM */
+        ScriptableObject.putProperty(scope, "initialDOM", initialDOM);
+      }
+
+      /* The event loop */
+      res = runScript(cx, scope, "event.js");
+      
+      /* Extract the rendered html from the VM's DOM */
+      res = cx.evaluateString(scope,"document.innerHTML","golf",0,null);
+      finalDOM = Context.toString(res);
+
+      String docType = 
+        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" " +
+        "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n";
+
+      /* Remove golfid attribute from the output, and html-escape '&' */
+      String clientDOM = 
+        finalDOM
+        .replaceAll("(<[^>]+) golfid=['\"][0-9]+['\"]", "$1")
+        .replaceAll("&", "&amp;");
+
+      out.println(docType + clientDOM);
+    }
+
+    catch (Exception x) {
+      response.setContentType("text/html");
+      out.println("<html><head><title>Golf Error</title></head><body>");
+      out.println("<table height='100%' width='100%'>");
+      out.println("<tr><td valign='middle' align='center'>");
+      out.println("<table width='600px'>");
+      out.println("<tr><td style='color:darkred;border:1px dashed red;" +
+                  "background:#fee;padding:0.5em;'>");
+      out.println("<b>Golf error:</b> " + HTMLEntityEncode(x.getMessage()));
+      out.println("</td></tr>");
+      out.println("</table>");
+      out.println("</td></tr>");
+      out.println("</table>");
+      out.println("</body></html>");
+    }
+
+    finally {
+      session.setAttribute("myScope", myScope);
+      if (finalDOM != null && finalDOM.length() >= 30) {
+        session.setAttribute("initialDOM", finalDOM);
+      }
+      Context.exit();
+      out.close();
+    }
+  }
+
+  private Object runScript(
+      Context cx,
+      Scriptable sc,
+      String path) 
+    throws IOException 
+  {
+    InputStream is = getGolfResourceAsStream(path);
+    InputStreamReader fs = new InputStreamReader(is);
+    return cx.evaluateReader(sc, fs, path, 1, null);
+  }
+
+  private String getPathRouted(String path) throws IOException {
+    String basename = "";
+    String dirname  = path;
+    
+    if (!path.endsWith("/")) {
+      dirname = "";
+      String segs[] = path.split("//*");
+      basename = segs[segs.length - 1];
+      for (int i=0; i<(segs.length - 1); i++)
+        dirname += segs[i] + "/";
+    }
+
+    if (dirname.equals("/") && basename.equals("routes.txt"))
+      return "/routes.txt";
+
+    BufferedReader sr = new BufferedReader(
+      new InputStreamReader(
+        getGolfResourceAsStream("/routes.txt")
+      )
+    );
+
+    String line;
+    while ( (line = sr.readLine()) != null ) {
+      String toks[] = line.split(":", 2);
+      String key    = toks[0];
+      String val    = toks[1];
+      dirname = dirname.trim();
+      dirname = dirname.replaceFirst("/*$", "/");
+      key     = key.trim();
+      key     = key.replaceFirst("/*$", "/");
+      val     = val.trim();
+      val     = val.replaceFirst("/*$", "/");
+      if (dirname.equals(key))
+        return val + basename;
+    }
+
+    throw new IOException("route not found: "+path);
+  }
+
+  private InputStream getGolfResourceAsStream(String name) throws IOException {
+    InputStream   is        = null;
+
+    String        libPath   = 
+      getServletContext().getRealPath("/libraries/" + name);
+    java.io.File  libFile   = new java.io.File(libPath);
+
+    java.io.File  cpnFile   = null;
+
+    try {
+      String cpnPath = 
+        getServletContext().getRealPath("/components" + getPathRouted(name));
+      cpnFile = new java.io.File(cpnPath);
+    } 
+
+    catch (Exception x) {
+      // no worries
+    }
+
+    String        thePath   = 
+      getServletContext().getRealPath(name);
+    java.io.File  theFile   = new java.io.File(thePath);
+
+    if (libFile.exists())
+      is = new FileInputStream(libFile);
+    else if (cpnFile != null && cpnFile.exists())
+      is = new FileInputStream(cpnFile);
+    else if (theFile.exists())
+      is = new FileInputStream(theFile);
+    else
+      is = getClass().getClassLoader().getResourceAsStream(name);
+
+    if (is == null)
+      throw new FileNotFoundException("File not found (" + name + ")");
+
+    return is;
+  }
+
+  private String getGolfResourceAsString(String name) throws IOException {
+    InputStream     is  = getGolfResourceAsStream(name);
+    BufferedReader  sr  = new BufferedReader(new InputStreamReader(is));
+    String          ret = "";
+
+    for (String s=""; (s = sr.readLine()) != null; )
+      ret = ret + s + "\n";
+
+    return ret;
+  }
+
+  private String fmtLogMsg(HttpServletRequest request, String s) {
+    HttpSession sess = request.getSession(false);
+    return (sess != null ? "[" + sess.getId().toUpperCase().replaceAll(
+          "(...)(?=...)", "$1.") + "]" : "") + " " + s;
+  }
+
+  private void logRequest(HttpServletRequest request) {
+    String method = request.getMethod();
+    String scheme = request.getScheme();
+    String server = request.getServerName();
+    int    port   = request.getServerPort();
+    String uri    = request.getRequestURI();
+    String query  = request.getQueryString();
+    String host   = request.getRemoteHost();
+
+    String line   = method + " " + (scheme != null ? scheme + ":" : "") +
+      "//" + (server != null ? server + ":" + port : "") +
+      uri + (query != null ? "?" + query : "") + " " + host;
+
+    Log.info(fmtLogMsg(request, line));
+  }
+
+  private void sendRedirect(
+    HttpServletRequest request,
+    HttpServletResponse response,
+    String uri
+  ) throws IOException {
+    Log.info(fmtLogMsg(request, "redirect -> `" + uri + "'"));
+    response.sendRedirect(uri);
+  }
+
+  public static String HTMLEntityEncode(String s) {
+    StringBuffer buf = new StringBuffer();
+    int len = (s == null ? -1 : s.length());
+
+    for ( int i = 0; i < len; i++ ) {
+      char c = s.charAt( i );
+      if ( c>='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9' ) {
+        buf.append( c );
+      } else {
+        buf.append("&#" + (int)c + ";");
+      }
+    }
+
+    return buf.toString();
+  }
+}
--- src/com/ubergibson/golf/Main.java
+++ src/com/ubergibson/golf/Main.java
@@ -1,6 +1,111 @@
 package com.ubergibson.golf;
-class Main {
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import gnu.getopt.Getopt;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.log.Log;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.HandlerContainer;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.handler.ContextHandlerCollection;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.handler.RequestLogHandler;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.servlet.DefaultServlet;
+import org.mortbay.thread.BoundedThreadPool;
+
+public class Main
+{
   public static void main(String[] args) {
-    System.out.println("golf says hi!");
+    // default port to listen on
+    Integer port = 8080;
+
+    HashMap<String, String> apps = new HashMap<String, String>();
+
+    // parse command line parameters
+    Getopt g = new Getopt("golf", args, "p:h");
+    int c;
+    while ((c = g.getopt()) != -1) {
+      switch (c) {
+        case 'p':
+          port = Integer.valueOf(g.getOptarg());
+          break;
+        case 'h':
+          // fall through
+        case '?':
+          usage();
+          break;
+      }
+    }
+
+    for (int i=g.getOptind(); i<args.length; i++) {
+      String newApp[] = args[i].split("@", 2);
+      apps.put(newApp[0], newApp[1]);
+    }
+
+    try {
+      Server server = new Server(port);
+      
+      BoundedThreadPool threadPool = new BoundedThreadPool();
+      threadPool.setMaxThreads(100);
+      server.setThreadPool(threadPool);
+
+      ContextHandlerCollection contexts = new ContextHandlerCollection();
+      
+      for (String app: apps.keySet()) {
+        Log.info("Starting app `" + app + "'");
+
+        String docRoot    = apps.get(app);
+
+        String golfPath   = "/" + app;
+        String golfRoot   = docRoot  + "/app";
+
+        String publicPath = golfPath + "/public";
+        String publicRoot = docRoot  + "/public/downloads";
+
+        Context cx1 = new Context(contexts, golfPath, Context.SESSIONS);
+        cx1.setResourceBase(golfRoot);
+        cx1.addServlet(new ServletHolder(new GolfServlet()), "/*");
+        
+        Context cx2 = new Context(contexts, publicPath, Context.SESSIONS);
+        cx2.setResourceBase(publicRoot);
+        DefaultServlet default_servlet = new DefaultServlet();
+        ServletHolder default_holder = new ServletHolder(default_servlet);
+        default_holder.setInitParameter(
+            "cacheControl", "max-age=3600, must-revalidate");
+        cx2.addServlet(default_holder, "/*");
+      }
+      
+      HandlerList handlers = new HandlerList();
+      handlers.setHandlers(new Handler[] {contexts, new DefaultHandler(),
+        new RequestLogHandler()});
+
+      server.setHandler(handlers);
+      server.setStopAtShutdown(true);
+      server.setSendServerVersion(true);
+
+      server.start();
+      server.join();
+    }
+    catch (Exception x) {
+      System.err.println( x.getMessage() );
+    }
+  }
+
+  private static void usage() {
+    System.out.println(
+        "Usage: java -jar golf.jar [-p port] [name@docroot]* name@docroot");
+    System.exit(1);
   }
 }
