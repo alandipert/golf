2ae9149e90a2d4cff6f01f859877a4a063489b80
Micha Niskin <asdf@ubergibson.com>
2008-11-16 21:56:45 +0000

    git-svn-id: svn+ssh://ubergibson.com/var/svn/repos/golf@10 9708d6e4-2587-447f-a9e4-23e61c6ca176

--- demo/app/components/org/golfscript/test/component.js
+++ demo/app/components/org/golfscript/test/component.js
@@ -1,36 +1,36 @@
-var count;
-Golf.title = "Golf Egg-speriment";
-
 function counter(n) {
   count = n;
   $("counter").text(String(count));
 }
 
-$("clickme").click(
-  function(event) {
-    counter(++count);
-    $("show").click();
-  }
-);
-
-$("show").click(
-  function(event) {
+// various states the component can be in:
+// 0: info is all shown
+// 1: info is hidden
+var states = [
+  function() {
     $("show").hide();
     $("close").show();
     $("reset").show();
     $("thanks").show();
-  }
-);
-
-$("close").click(
-  function(event) {
+  },
+  function() {
     $("thanks").hide();
     $("close").hide();
     $("reset").hide();
     $("show").show();
+  },
+];
+    
+// clicking on the heading causes the counter to increment
+// and info to be shown
+$("clickme").click(
+  function(event) {
+    counter(++count);
+    states[0]();
   }
 );
 
+// clicking on the reset button causes the counter to reset to zero
 $("reset").click(
   function(event) {
     counter(0);
@@ -38,8 +38,22 @@ $("reset").click(
   }
 );
 
-counter(0);
-$("thanks").hide();
-$("reset").hide();
-$("close").hide();
-$("show").show();
+// click the "more info" button to show more info
+$("show").click(
+  function(event) {
+    states[0]();
+  }
+);
+
+// click on the "less info" button to show less info
+$("close").click(
+  function(event) {
+    states[1]();
+  }
+);
+
+var count; // pseudo-global variable to hold some state info
+Golf.title = "Golf Egg-speriment"; // set the page title
+
+counter(0);  // initialize the counter
+states[1](); // set state 1, i.e. info hidden
