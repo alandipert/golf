b90396ac54c357d986da4a0a1eed2fe85bf9c5cf
Micha Niskin <asdf@ubergibson.com>
2009-02-21 00:15:20 -0500

    minor bugfixes

--- examples/helloworld/components/com/thinkminimo/hello.js
+++ examples/helloworld/components/com/thinkminimo/hello.js
@@ -1,8 +1,8 @@
 
 function(argv) {
-  $(".relatives").after("<a href='oh/dear/'>oh/dear/</a><br/>");
-  $(".relatives").after("<a href='/you/didnt/'>/you/didnt/</a><br/>");
-  $(".relatives").after("<a href='#well/heck/yes/'>#home/heck/yes/</a><br/>");
   $(".absolutes")
     .after("<a href='http://thinkminimo.com'>http://thinkminimo.com</a><br/>");
+  $(".relatives").after("<a href='oh/dear/'>oh/dear/</a><br/>");
+  $(".relatives").after("<a href='/you/didnt/'>/you/didnt/</a><br/>");
+  $(".relatives").after("<a href='#well/heck/yes/'>#well/heck/yes/</a><br/>");
 }
